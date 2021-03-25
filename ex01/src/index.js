3f57e598bcc7e6515b19c573945b8355b0f89835
Kemal Arifovic <gigalab@gmail.com>
2021-03-25 15:17:10 +0100

    Generator Functions

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,9 +1,8 @@
 let myArr = [];
 // Only change code below this line
 function* multiplication(x) {
-  const X = x;
-  for (let i = 0; i < X; i++) {
-    yield (x = x * 2);
+  for (let i = x; i >= 0; i--) {
+    yield (x *= 2);
     myArr.push(x);
   }
 }
@@ -11,8 +10,8 @@ function* multiplication(x) {
 let num = multiplication(3);
 
 for (let i = 0; i < 3; i++) {
-  let { value } = num.next();
-  console.log(value);
+  
+  console.log(num.next().value);
 }
 // Only change code above this line
 module.exports = multiplication;
